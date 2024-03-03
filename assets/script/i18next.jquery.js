572c04d8fe23cf1068aa3ceccb482758c745e794
Tamas Dohany <iamtamasdohany@gmail.com>
2024-03-03 11:18:12 +0100

    localizer hu|en

---
+++ assets/script/i18next.jquery.js
@@ -0,0 +1,34 @@
+$(function () {
+  i18next
+    .use(i18nextBrowserLanguageDetector)
+    .init({
+      debug: true,
+      fallbackLng: 'en',
+      lng: 'en',
+      resources: {
+        en: {
+          translation: {
+            modal: {
+              text: "under construction..."
+            },
+          }
+        },
+        hu: {
+          translation: {
+            modal: {
+              text: "előkészítés alatt..."
+            },
+          }
+        }
+      }
+    }, function (err, t) {
+      jqueryI18next.init(i18next, $, { useOptionsAttr: true });
+      $('body').localize();
+    });
+  $('.hu').on('click', function () {
+    $('body').localize({ lng: 'hu' });
+  });
+  $('.en').on('click', function () {
+    $('body').localize({ lng: 'en' });
+  });
+});
