1a82fd2575179ef6636d0d92cdcd6867a99e167d
Tamas Dohany <iamtamasdohany@gmail.com>
2024-03-05 12:01:33 +0100

    style & fn issue

--- assets/script/i18next.jquery.js
+++ assets/script/i18next.jquery.js
@@ -25,12 +25,26 @@ $(function () {
       jqueryI18next.init(i18next, $, { useOptionsAttr: true });
       $('body').localize();
     });
+
   $('.hu').on('click', function () {
     $('body').localize({ lng: 'hu' });
+    $(".language a").on('click', function () {
+      $(".language a").each(function () {
+        $(this).removeClass('active');
+      });
+      $(this).addClass('active');
+    });
     return false;
   });
+
   $('.en').on('click', function () {
     $('body').localize({ lng: 'en' });
+    $(".language a").on('click', function () {
+      $(".language a").each(function () {
+        $(this).removeClass('active');
+      });
+      $(this).addClass('active');
+    });
     return false;
   });
 });
